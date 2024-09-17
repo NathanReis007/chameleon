eddfbd5941a25203a01c23159fd39515aff9a11c
NathaReis <153759333+NathaReis@users.noreply.github.com>
2024-09-17 18:08:52 -0300

    Design

---
+++ src/script.js
@@ -0,0 +1,13 @@
+const $logo = document.querySelector("#logo");
+const $menu = document.querySelector("#menu");
+const $menuInfo = document.querySelector("#menuInfo");
+const toggleMenu = (isToggle=true) => {
+    if(isToggle) {
+        $menu.classList.toggle("activated");
+        $menuInfo.classList.toggle("activated");
+    }
+    else {
+        $menu.classList.remove("activated");
+        $menuInfo.classList.add("activated");
+    }
+};
