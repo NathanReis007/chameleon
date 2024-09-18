be5fb19556aa1580923691d098b0b88e6a21670d
NathaReis <153759333+NathaReis@users.noreply.github.com>
2024-09-18 14:35:13 -0300

    Init bt-filtro

--- src/script.js
+++ src/script.js
@@ -5,9 +5,11 @@ const toggleMenu = (isToggle=true) => {
     if(isToggle) {
         $menu.classList.toggle("activated");
         $menuInfo.classList.toggle("activated");
+        $logo.classList.toggle("activated");
     }
     else {
         $menu.classList.remove("activated");
         $menuInfo.classList.add("activated");
+        $logo.classList.remove("activated");
     }
 };
