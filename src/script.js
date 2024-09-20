3a0786f3ba545fbfa4b06f3ef9fb03187d69703c
NathaReis <153759333+NathaReis@users.noreply.github.com>
2024-09-20 17:07:05 -0300

    Caixas de dialog

--- src/script.js
+++ src/script.js
@@ -13,3 +13,26 @@ const toggleMenu = (isToggle=true) => {
         $logo.classList.remove("activated");
     }
 };
+
+const $filtro = document.querySelector("#filtro");
+const toggleFiltro = (menu) => {
+    $filtro.classList.toggle("activated");
+    if(menu) {
+        toggleMenu();
+    }
+}
+
+const $menuConta = document.querySelector("#menuConta");
+const toggleMenuConta = (isToggle=true) => {
+    if(isToggle) {
+        $menuConta.classList.toggle("activated");
+    }
+    else {
+        $menuConta.classList.remove("activated");
+    }
+}
+
+const closeMenus = () => {
+    toggleMenu(false);
+    toggleMenuConta(false);
+}
